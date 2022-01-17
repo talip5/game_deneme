f6d5f26ebed52aa264554316c77297937bc1028a
talip <talipefe35@gmail.com>
2022-01-17 23:53:34 +0300

    17.01.2022 step 5

--- lib/components/agile-fly.dart
+++ lib/components/agile-fly.dart
@@ -1,12 +1,16 @@
+import 'dart:ui';
 import 'package:flame/sprite.dart';
 import 'package:game_deneme/langawGame.dart';
 import 'package:game_deneme/components/fly.dart';
 
 class AgileFly extends Fly {
-  AgileFly(LangawGame game, double x, double y) : super(game, x, y) {
+  double get speed => game.tileSize * 5;
+
+  AgileFly(LangawGame game, double x, double y) : super(game) {
     flyingSprite = List();
     flyingSprite.add(Sprite('flies/agile-fly-1.png'));
     flyingSprite.add(Sprite('flies/agile-fly-2.png'));
     deadSprite = Sprite('flies/agile-fly-dead.png');
+    flyRect = Rect.fromLTWH(x, y, game.tileSize * 1.5, game.tileSize * 1.5);
   }
 }
